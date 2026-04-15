fcaa7d1ba40bc1b168b347f059a365bce3dc51a8
Gato81-117 <saulct001@gmail.com>
2026-04-15 15:02:39 -0600

    Fix phone number format in WhatsApp link

--- script.js
+++ script.js
@@ -328,7 +328,7 @@ function sendToWhatsApp() {
     }
     
     const encodedMessage = encodeURIComponent(message);
-    window.open(`https://wa.me/522223947541?text=${encodedMessage}`, '_blank');
+    window.open(`https://wa.me/+52 1 222 394 7541?text=${encodedMessage}`, '_blank');
 }
 
 function toggleModal(open) {

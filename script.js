65f2c14da30bfeaa03dcde4a13482e290246b760
Gato81-117 <saulct001@gmail.com>
2026-04-15 15:47:50 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -328,7 +328,7 @@ function sendToWhatsApp() {
     }
 
     const encodedMessage = encodeURIComponent(message);
-    window.open(`https://wa.me/5212223947541?text=${encodedMessage}`, '_blank');
+    window.open(`https://wa.me/522221957322?text=${encodedMessage}`, '_blank');
 }
 
 function toggleModal(open) {
